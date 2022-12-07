b6d46e7e2ede20263c4843452ddd48d495520cbb
Prabhat Dwivedie <64556857+prabhatpp@users.noreply.github.com>
2022-12-07 12:51:03 +0530

    Add files via upload

---
+++ lambda/lammbda-iam.tf
@@ -0,0 +1,42 @@
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda_role"
+  
+
+  assume_role_policy = <<EOF
+  {
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "lambda_policy" {
+  name        = "lambda_policy"
+  description = "A test policy"
+
+ policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "arn:aws:logs:*:*:*",
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
